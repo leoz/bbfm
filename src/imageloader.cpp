9a752efd926c26244912925006c1d4f9ccb4b442
leoz2011 <leonid.zolotarev@yahoo.com>
2013-09-16 09:07:02 -0700

    Show human readable file size

--- src/imageloader.cpp
+++ src/imageloader.cpp
@@ -216,7 +216,19 @@ QString ImageLoader::getDirSize() const
 
 QString ImageLoader::getFileSize() const
 {
-	return QString::number(m_fileInfo.size());
+     float num = m_fileInfo.size();
+     QStringList list;
+     list << "KB" << "MB" << "GB" << "TB";
+
+     QStringListIterator i(list);
+     QString unit("bytes");
+
+     while(num >= 1024.0 && i.hasNext()) {
+         unit = i.next();
+         num /= 1024.0;
+     }
+
+     return QString().setNum(num,'f',0)+" "+unit;
 }
 
 void ImageLoader::setDefaultImage()
